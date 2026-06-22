a8218c933c362c0834ffd8516aa868374faa4470
yugpa <yugpatel81288@gmail.com>
2026-06-22 14:30:40 +0530

    i

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+
+fun main() {
+
+    var i=1
+    var j:Int = 7
+    var k:Float = 7.9F
+    var l:Char = 'T'
+    var m:String = "HMS"
+    var n:Boolean = false
+    var o:Double = 96.36
+    var p:Long = 338927847
+    var q:Short = -2
+    var r:Byte = 127
+    println("r="+r)
+    println("q="+q)
+    println("p="+p)
+    println("o="+o)
+    println("n="+n)
+    println("m="+m)
+    println("l="+l)
+    println("k="+k)
+    println("j="+j)
+    println("i="+i)
+}
